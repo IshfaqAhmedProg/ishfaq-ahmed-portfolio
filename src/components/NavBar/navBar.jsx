32f8a8299e482fe29b10fec979c0e8d1a48de5d7
IshfaqAhmedProg <manslayeriam@gmail.com>
2022-04-30 09:03:24 +0600

    going home

---
+++ src/components/NavBar/navBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../NavBar/navBar.scss'
+
+function Navbar() {
+    return (
+        <div className='navBar-container'>
+            
+        </div>        
+    )
+}
+
+export default Navbar
